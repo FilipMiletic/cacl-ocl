42632747fd57693a9ac3c4d9ec2c1a91e2bf775b
Filip Miletic <filip.miletic@ims.uni-stuttgart.de>
2025-01-27 18:13:42 +0100

    update grobid processing

--- get_xml_from_pdf.py
+++ get_xml_from_pdf.py
@@ -3,9 +3,6 @@ import os
 from grobid.client import GrobidClient
 
 def process_pdfs(in_dir, out_dir):
-    
-    # Grobid client needs to be running on Docker
-    client = GrobidClient(port='8070')
 
     pdf_files = os.listdir(in_dir)
     pdf_files = [f for f in pdf_files if f.endswith('.pdf')]
@@ -13,19 +10,25 @@ def process_pdfs(in_dir, out_dir):
     print(f'Processing {len(pdf_files)} PDF files.')
     print(f'Input directory:  {in_dir}')
     print(f'Output directory: {out_dir}')
+
+    # Grobid client needs to be running on Docker
+    client = GrobidClient(port='8070')
     
     for i, pdf_file in enumerate(pdf_files):
+
+        xml_file = f'{pdf_file[:-4]}.tei.xml'
+        xml_file = os.path.join(out_dir, xml_file)
+        
+        if os.path.isfile(xml_file):
+            print(f'Skipping existing file: {xml_file}')
+            continue
+            
         rsp = client.serve(service='processFulltextDocument',
                            pdf_file=os.path.join(in_dir, pdf_file))
         text = rsp[0].content
 
-        xml_file = f'{pdf_file[:-4]}.tei.xml'
-        xml_file = os.path.join(out_dir, xml_file)
-        if not os.path.isfile(xml_file):
-            with open(xml_file, 'wb') as f:
-                f.write(text)
-        else:
-            print(f'Skipping existing file: {xml_file}')
+        with open(xml_file, 'wb') as f:
+            f.write(text)
             
         if i % 100 == 0:
             print(f'Processed {i} files.')
